62ca1536724b59158887f017a05bf39b1261498e
Paul Mauviel <pmauviel@imvu.com>
2021-11-08 13:58:03 -0500

    Inital commit with the submodules

---
+++ cmake-build-debug/CMakeFiles/gamejam01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gamejam01.dir/main.cpp.o"
+  "gamejam01"
+  "gamejam01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gamejam01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
